bf4545f536c5ae575e82e38f214e36070859175b
n4inz <zulkar.n4inz@gmail.com>
2023-10-15 09:20:53 +0800

    bulanan

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -19,6 +19,13 @@
         <div class="box">
             <div class="box-header with-border">
                 <button onclick="updatePeriode()" class="btn btn-info btn-xs btn-flat"><i class="fa fa-plus-circle"></i> Ubah Periode</button>
+                
+                <select id="select-day" class="btn btn-info btn-xs btn-flat" name="" id="">
+                    <option value="">--Pilih--</option>
+                    <option value="harian">Harian</option>
+                    <option value="bulanan">Bulanan</option>
+                    <option value="tahunan">Tahunan</option>
+                </select>
                 <a href="{{ route('laporan.export_pdf', [$tanggalAwal, $tanggalAkhir]) }}" target="_blank" class="btn btn-success btn-xs btn-flat"><i class="fa fa-file-excel-o"></i> Export PDF</a>
             </div>
             <div class="box-body table-responsive">
@@ -74,5 +81,33 @@
     function updatePeriode() {
         $('#modal-form').modal('show');
     }
+
+
+    $('#select-day').change(function() {
+    var val = $(this).val();
+    var currentDate = new Date().toISOString().split('T')[0]; // Mendapatkan tanggal saat ini dalam format "YYYY-MM-DD"
+    var newUrl = 'http://127.0.0.1:8000/laporan?';
+
+    if (val == 'harian') {
+        newUrl += 'tanggal_awal=' + currentDate + '&tanggal_akhir=' + currentDate;
+    } else if (val == 'bulanan') {
+        var today = new Date();
+        var firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+        var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        var firstDayFormatted = firstDayOfMonth.toISOString().split('T')[0];
+        var lastDayFormatted = lastDayOfMonth.toISOString().split('T')[0];
+        newUrl += 'tanggal_awal=' + firstDayFormatted + '&tanggal_akhir=' + lastDayFormatted;
+    } else if (val == 'tahunan') {
+        var today = new Date();
+        var firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+        var lastDayOfYear = new Date(today.getFullYear(), 11, 31);
+        var firstDayFormatted = firstDayOfYear.toISOString().split('T')[0];
+        var lastDayFormatted = lastDayOfYear.toISOString().split('T')[0];
+        newUrl += 'tanggal_awal=' + firstDayFormatted + '&tanggal_akhir=' + lastDayFormatted;
+    }
+
+    // Redirect ke URL baru
+    window.location.href = newUrl;
+});
 </script>
 @endpush
