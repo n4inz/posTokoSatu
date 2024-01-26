7ecd2b473c5842768b7a74d84e4a0e3b1360c86a
n4inz <zulkar.n4inz@gmail.com>
2024-01-26 21:16:20 +0800

    los

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -79,7 +79,9 @@
     <div class="col-lg-12">
         <div class="box">
             <div class="box-header with-border">
-                <h3 class="box-title">Grafik Pendapatan {{ tanggal_indonesia($tanggal_awal, false) }} s/d {{ tanggal_indonesia($tanggal_akhir, false) }}</h3>
+                <h3 class="box-title">Grafik Penjualan 2019 s/d 2024</h3>
+
+                {{-- <h3 class="box-title">Grafik Penjualan {{ tanggal_indonesia($tanggal_awal, false) }} s/d {{ tanggal_indonesia($tanggal_akhir, false) }}</h3> --}}
             </div>
             <!-- /.box-header -->
             <div class="box-body">
@@ -105,35 +107,66 @@
 @push('scripts')
 <!-- ChartJS -->
 <script src="{{ asset('AdminLTE-2/bower_components/chart.js/Chart.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <script>
 $(function() {
-    // Get context with jQuery - using jQuery's .get() method.
-    var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
-    // This will get the first returned node in the jQuery collection.
-    var salesChart = new Chart(salesChartCanvas);
+    // // Get context with jQuery - using jQuery's .get() method.
+    // var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
+    // // This will get the first returned node in the jQuery collection.
+    // var salesChart = new Chart(salesChartCanvas);
 
-    var salesChartData = {
-        labels: {{ json_encode($data_tanggal) }},
-        datasets: [
-            {
-                label: 'Pendapatan',
-                fillColor           : 'rgba(60,141,188,0.9)',
-                strokeColor         : 'rgba(60,141,188,0.8)',
-                pointColor          : '#3b8bba',
-                pointStrokeColor    : 'rgba(60,141,188,1)',
-                pointHighlightFill  : '#fff',
-                pointHighlightStroke: 'rgba(60,141,188,1)',
-                data: {{ json_encode($data_pendapatan) }}
-            }
-        ]
-    };
+    // var salesChartData = {
+    //     // labels: {{ json_encode($data_tanggal) }},
+    //     labels: ['2019', '2020', '2021', '2022', '2023', '2024'],
 
-    var salesChartOptions = {
-        pointDot : false,
-        responsive : true
-    };
+    //     datasets: [
+    //         {
+    //             label: 'Penjualan',
+    //             // fillColor           : 'rgba(60,141,188,0.9)',
+    //             // strokeColor         : 'rgba(60,141,188,0.8)',
+    //             // pointColor          : '#3b8bba',
+    //             // pointStrokeColor    : 'rgba(60,141,188,1)',
+    //             // pointHighlightFill  : '#fff',
+    //             // pointHighlightStroke: 'rgba(60,141,188,1)',
+    //             // data: {{ json_encode($data_pendapatan) }}
+    //             data: [20000000, 23000000, 18000000, 15000000, 30000000, 5000000],
 
-    salesChart.Line(salesChartData, salesChartOptions);
+    //         }
+    //     ]
+    // };
+
+    // var salesChartOptions = {
+    //     // pointDot : false,
+    //     responsive : true
+    // };
+
+    // salesChart.Line(salesChartData, salesChartOptions);
+
+
+
 });
+
+    const ctx = document.getElementById('salesChart');
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['2019', '2020', '2021', '2022', '2023', '2024'],
+            datasets: [{
+            label: 'Penjualan',
+            data: [20000000, 23000000, 18000000, 15000000, 30000000, 5000000],
+            borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+            y: {
+                beginAtZero: true
+            }
+            }
+        }
+    });
+
 </script>
 @endpush
